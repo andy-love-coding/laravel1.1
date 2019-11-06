ee027484aae536e3c7ca5c2f87abfb3d1d379184
andy <andy.liujin@foxmail.com>
2019-11-06 04:16:53 +0000

    发布微博

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]); // 这样创建的微博会自动与用户进行关联。
+
+        session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+}
